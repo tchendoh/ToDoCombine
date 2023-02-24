e82effbd73e088bdaf1e7f2d5484c0699f8237b3
tchendoh <42325308+tchendoh@users.noreply.github.com>
2023-02-24 08:33:49 -0500

    Initial Commit

---
+++ combineToDo/combineToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  combineToDoApp.swift
+//  combineToDo
+//
+//  Created by Eric Chandonnet on 2023-02-24.
+//
+
+import SwiftUI
+
+@main
+struct combineToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
